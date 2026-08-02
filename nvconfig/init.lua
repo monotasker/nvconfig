af21ac90bc6f4106fa37e929d0512a4373542237
Ian W. Scott <scottianw@gmail.com>
2026-08-02 03:03:54 -0400

    Fix color initialization issue with Themery

--- nvconfig/init.lua
+++ nvconfig/init.lua
@@ -4,6 +4,10 @@ vim.g.node_path = os.getenv("NVM_DIR") .. "/versions/node/v24.9.0/bin/node"
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
+-- Must be set before Themery/lazy loads the colorscheme, otherwise GUI
+-- highlights are skipped on first paint.
+vim.opt.termguicolors = true
+
 require("nvconfig.lazy_init")
 require("nvconfig.options")
 require("nvconfig.remap")
