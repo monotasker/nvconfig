6343ceaf5cd1204ab52d4e7a27038c81bd425279
Ian W. Scott <scottianw@gmail.com>
2026-09-21 19:50:28 -0400

    fix(neogit): Minor config tweaks

--- nvconfig/plugins/neogit.lua
+++ nvconfig/plugins/neogit.lua
@@ -2,14 +2,18 @@
 -- Installed via store.nvim
 
 return {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim", -- required
-        "sindrets/diffview.nvim", -- optional - Diff integration
-        -- Only one of these is needed.
-        "nvim-telescope/telescope.nvim", -- optional
-        "ibhagwan/fzf-lua", -- optional
-        "echasnovski/mini.pick", -- optional
-        "folke/snacks.nvim" -- optional
-    }
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua", -- optional
+    "echasnovski/mini.pick", -- optional
+    "folke/snacks.nvim", -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
 }
